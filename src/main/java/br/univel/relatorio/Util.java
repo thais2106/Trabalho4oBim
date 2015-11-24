5f2d24049bc49299116f218db07701d0c742055e
tcrivelatti <tcrivelatti@ws-lab01_150.univel.univel.br>
2015-11-24 21:42:39 -0200

    Teste de relatório do Cliente.

---
+++ src/main/java/br/univel/relatorio/Util.java
@@ -0,0 +1,42 @@
+package br.univel.relatorio;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import br.univel.conexao.ConexaoServidor;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+
+public class Util {
+
+	public static void geraRelatorioEmPdfConsulta(String SQL, String jasper, String nomeRelatorio) {
+		try {
+			String saida = null;
+			Map<String, Object> map = new HashMap<String, Object>();
+			map.put("SQL", SQL);
+
+			InputStream jasperTemplate = Util.class.getClass().getResourceAsStream(jasper);
+			
+			JasperPrint print = JasperFillManager.fillReport(jasperTemplate, map, ConexaoServidor.getConnection());
+			saida = "C:\\Users\\tcrivelatti\\Downloads\\" + nomeRelatorio + ".pdf";
+			JasperExportManager.exportReportToPdfFile(print, saida);
+			JOptionPane.showMessageDialog(null, "<html>Arquivo exportado para PDF!<br><br>A aplicação vai pedir"
+					+ " ao Sistema operacional <br>para abrir com o visualizador" + " padrão.");
+
+			Desktop.getDesktop().open(new File(saida));
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JRException e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
